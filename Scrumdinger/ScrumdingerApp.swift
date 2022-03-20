dc4aa08290036212442a130cb934585ab4324d76
Sebastiano Cassol <cassol.sebastiano@gmail.com>
2022-03-20 13:59:51 +0100

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Sebastiano Cassol on 20/03/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
